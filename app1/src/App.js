9b592d7f291d6eee669f93cd5c75183031704efc
Ripunjoy Sarkar <ripsarkar@gmail.com>
2023-03-18 18:53:27 +0530

    please add

---
+++ app1/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import {Buttons} from 'core';
+export function App() {
+  return (
+    <div className="App">
+      <Buttons />
+    </div>
+  );
+}
---
+++ app2/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+export function AppFrom2() {
+  return (
+    <div className="App">
+      App2 visuals
+    </div>
+  );
+}
